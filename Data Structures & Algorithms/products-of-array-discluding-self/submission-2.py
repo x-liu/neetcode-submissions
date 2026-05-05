5bb7134138ea81474ea2b20a13b106b0e4b4f5ac
x-liu <xingliu321@gmail.com>
2026-05-05 09:11:32 -0400

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        nZeros = 0
+        maxNum = 1
+        for num in nums:
+            if num == 0:
+                nZeros+=1
+            else:
+                maxNum*=num
+
+        out = [0] *len(nums)
+        if nZeros == 1:
+            for i, num in enumerate(nums):
+                if num == 0:
+                    out[i] = maxNum
+                    return out
+        if nZeros == 0:
+            out = [1] * len(nums)
+            for i, num in enumerate(nums):
+                out[i] = maxNum//num
+        return out
+
+
+
+
